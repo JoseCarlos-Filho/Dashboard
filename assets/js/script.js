b79a17e7e8dd31c31268e7029cc0721d946fa3ff
José Carlos <etijcvivi@gmail.com>
2022-08-13 22:22:12 -0300

    Projeto Dashboard - Montagem do Header - add event click on nav-toggle-btn.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* 
+* ativa a classe toggle no header. 
+* quando clicado em nav-toggle-btn
+*/
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+navToggleBtn.addEventListener("click", () => {
+    header.classList.toggle("active");
+});
