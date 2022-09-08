9c8dcd1c57f27e9abe8c67213d101ba43ecc6a03
José Carlos <etijcvivi@gmail.com>
2022-09-08 00:27:52 -0300

    Dashboard - Header da page concluido - inicio do corpo da page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-'use strict';
+"use strict";
 
-/* 
-* ativa a classe toggle no header. 
-* quando clicado em nav-toggle-btn
-*/
+/*
+ * ativa a classe toggle no header.
+ * quando clicado em nav-toggle-btn
+ */
 
 const header = document.querySelector("[data-header]");
 const navToggleBtn = document.querySelector("[data-menu-toggle-btn]");
-console.log(header);
+// console.log(header);
 
-navToggleBtn.addEventListener("click", () => {
-    header.classList.toggle("active");
-    console.log(header.classList.toggle("active"));
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
 });
